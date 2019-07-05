1bc58340ec0f65cc91b40658e8dce190529ece23
Erik Lopez <maileks@gmail.com>
2019-07-05 17:46:26 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,22 @@
 from setuptools import setup, find_packages
 
-def get_requirements():
-    requirements = []
-    with open('requirements.txt', 'r') as req_file:
-        source = req_file.read()
-        if source:
-            requirements = source.splitlines()
-    return requirements
+with open('README.md', 'r') as f:
+    README = f.read()
 
 setup(
     name='instpector',
-    version='0.1.0',
+    version='0.1.1',
     description='The Instagram web command line interface',
     author='Erik Lopez',
+    long_description=README,
+    long_description_content_type="text/markdown",
     url='https://github.com/niuware/instpector',
-    download_url='https://github.com/niuware/instpector/archive/0.1.0.tar.gz',
+    download_url='https://github.com/niuware/instpector/archive/0.1.1.tar.gz',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     packages=find_packages(exclude=('examples', 'tests')),
-    install_requires=get_requirements()
+    install_requires=['requests']
 )
